4393fdee733c99e9cff9b2e3ef9b82996b56c5ed
David Crooks <crodave@gmail.com>
2020-09-10 17:10:31 +0200

    Initial Commit

---
+++ ARChess/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARChess
+//
+//  Created by David Crooks on 10/09/2020.
+//  Copyright © 2020 David Crooks. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
